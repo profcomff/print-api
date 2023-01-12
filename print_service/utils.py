26d8418e5736da68b4741bb684de806d58e32a5b
nmkazantsev <nmkazantsev@gmail.com>
2023-01-12 20:46:22 +0300

    update file utils

--- print_service/utils.py
+++ print_service/utils.py
@@ -1,16 +1,14 @@
 import random
-import re
 from datetime import date, datetime, timedelta
-import PIL.Image
+from PIL import Image
 from sqlalchemy.orm.session import Session
 
 from print_service.models import File
 from print_service.settings import Settings, get_settings
-
-from os import remove
+from os.path import  splitext
 import time
 from io import BytesIO
-
+from fpdf import FPDF
 
 settings: Settings = get_settings()
 
@@ -35,15 +33,28 @@ def generate_pin(session: Session):
 def generate_filename(original_filename: str):
     datestr = date.today().isoformat()
     salt = ''.join(random.choice(settings.PIN_SYMBOLS) for i in range(128))
-    ext = re.findall(r'\w+', original_filename.split('.')[-1])[0]
+    ext = splitext(original_filename)[1]
     return f'{datestr}-{salt}.{ext}'
 
 
 #преобразует файл из картинки в pdf
-def process_image(file,newFileName):
-    image=PIL.Image.open(BytesIO(file)).convert("RGB")  
-    if(image.width<image.height):
-        image=image.rotate(90,expand=True)
-    image.save(f"{newFileName}.pdf", "PDF", quality=100)
-    #remove(filepath)# удаляем старый файл - картинку
+def process_image(file :str,newFileName :str):
+    img=Image.open(BytesIO(file)).convert("RGB")  
+    if(img.width>img.height):
+        img=img.rotate(90,expand=True)
+    #img.save(f"{newFileName}.pdf", "PDF", quality=100)
+    pdf = FPDF()
+    pdf.add_page()
+
+    FPDF.set_left_margin(pdf,0)
+    FPDF.set_right_margin(pdf,0)
+    FPDF.set_top_margin(pdf,10)
+    
+    k=min(pdf.eph/img.height,pdf.epw/img.width)
+    wid=img.width*k
+    heig=img.height*k
+    #print(wid,heig,pdf.epw,pdf.eph, (pdf.eph-heig)/2)
+    pdf.image(img, x=(pdf.epw-wid)/2,y=(pdf.eph-heig)/2+15,h=heig, w=wid) 
+    pdf.output(f"{newFileName}pdf")
+
         

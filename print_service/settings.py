2587699073f84e6a19eb33da2021e016a329ef62
semen603089 <semen603089@mail.ru>
2023-03-26 15:05:01 +0300

    black

--- print_service/settings.py
+++ print_service/settings.py
@@ -35,8 +35,6 @@ class Settings(UnionAuthSettings, BaseSettings):
     QR_TOKEN_TTL: int = 30  # Show time of QR code in seconds
     QR_TOKEN_DELAY: int = 5  # How long QR code valid after hide in seconds
 
-    AUTH_URL: AnyUrl = "https://auth.api.test.profcomff.com/"
-
     class Config:
         env_file = '.env'
 

6becd9e1ae22b31b8d685c8bdb104c4a414d69af
gitfresnel <151745312+gitfresnel@users.noreply.github.com>
2024-11-06 00:12:08 +0300

    f

--- tests/test_routes/test_user.py
+++ tests/test_routes/test_user.py
@@ -5,6 +5,7 @@ from starlette import status
 
 from print_service.models import UnionMember
 from print_service.settings import get_settings
+from sqlalchemy import and_, func
 
 
 url: str = '/is_union_member'
@@ -46,14 +47,14 @@ def test_post_success(client, dbsession):
                 'union_number': '1966',
                 'student_number': '1967',
             }
-        ],
+        ]
     }
     res = client.post(url, data=json.dumps(body))
     assert res.status_code == status.HTTP_200_OK
-    UnionMember.query(session=dbsession).filter(
-        UnionMember.surname == body['users'][0]['username'],
-        UnionMember.union_number == body['users'][0]['union_number'],
-        UnionMember.student_number == body['users'][0]['student_number'],
+    UnionMember.query(session=dbsession).filter(and_(
+        UnionMember.surname == func.upper(body['users'][0]['username']),
+        UnionMember.union_number == func.upper(body['users'][0]['union_number']),
+        UnionMember.student_number == func.upper(body['users'][0]['student_number']))
     ).delete()
     dbsession.commit()
 
@@ -66,10 +67,54 @@ def test_post_is_deleted(client, union_member_user, add_is_deleted_flag):
                 'union_number': '6666667',
                 'student_number': '13033224',
             }
-        ],
+        ]
     }
     res = client.post(url, data=json.dumps(body))
     assert res.status_code == status.HTTP_404_NOT_FOUND
+
+
+def test_restore_is_deleted(client, dbsession):
+    user = UnionMember(id=5,
+                       surname='test_user',
+                       union_number='123',
+                       student_number='56',
+                       is_deleted=False)
+    dbsession.add(user)
+    dbsession.commit()
+
+    body = {
+        'users': [
+            {
+                'username': 'test_user',
+                'union_number': '123',
+                'student_number': '56',
+                'is_deleted': True
+             }
+        ]
+    }
+    _ = client.post(url, data=json.dumps(body))
+    res = UnionMember.query(session=dbsession, with_deleted=True).filter(UnionMember.id == 5).one_or_none()
+    assert res.is_deleted is False
+    user.is_deleted = True
+    dbsession.commit()
+    body = {
+        'users': [
+            {
+                'username': 'test_user',
+                'union_number': '123',
+                'student_number': '56',
+                'is_deleted': False
+            }
+        ]
+    }
+    res = client.post(url, data=json.dumps(body))
+    assert res.status_code == status.HTTP_404_NOT_FOUND
+    UnionMember.query(session=dbsession, with_deleted=True).filter(and_(
+        UnionMember.surname == func.upper(body['users'][0]['username']),
+        UnionMember.union_number == func.upper(body['users'][0]['union_number']),
+        UnionMember.student_number == func.upper(body['users'][0]['student_number'])
+    )).delete()
+    dbsession.commit()
 
 
 @pytest.mark.parametrize(
